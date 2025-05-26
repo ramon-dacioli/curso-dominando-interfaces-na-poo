9923435cfa5803d039dfafcf0279a66f3e0dd349
Ramon Dacioli <ramon_muz@hotmail.com>
2025-05-26 11:40:15 -0300

    Trocando a regra de negócio sem mexer na interface

--- eInterface.View.Principal.dfm
+++ eInterface.View.Principal.dfm
@@ -10,6 +10,7 @@ object Form1: TForm1
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
+  OnCreate = FormCreate
   TextHeight = 15
   object Button1: TButton
     Left = 8
--- eInterface.View.Principal.pas
+++ eInterface.View.Principal.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,
+  eInterface.Model.Interfaces;
 
 type
   TForm1 = class(TForm)
@@ -13,8 +14,10 @@ type
     Edit2: TEdit;
     Memo1: TMemo;
     procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
+    FPessoa : iPessoa;
   public
     { Public declarations }
   end;
@@ -25,19 +28,23 @@ var
 implementation
 
 uses
-  eInterface.Model.Pessoa;
+  eInterface.Model.PessoaFisica, eInterface.Model.PessoaJuridica;
 
 {$R *.dfm}
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   Memo1.Lines.Add(
-  TModelPessoa
-    .New
+  FPessoa
     .Nome(Edit1.Text)
     .SobreNome(Edit2.Text)
     .NomeCompleto
   );
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FPessoa := TModelPessoaJuridica.New;
 end;
 
 end.
