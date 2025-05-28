bf7735751b54228b7dfec53cdd73c1b87ccf3df2
Ramon Dacioli <ramon_muz@hotmail.com>
2025-05-28 14:43:15 -0300

    Adicionando arquivo readme.me e incluindo descrição

--- Project1.dpr
+++ Project1.dpr
@@ -4,7 +4,11 @@ uses
   Vcl.Forms,
   eInterface.View.Principal in 'eInterface.View.Principal.pas' {Form1},
   eInterface.Model.Interfaces in 'eInterface.Model.Interfaces.pas',
-  eInterface.Model.Pessoa in 'eInterface.Model.Pessoa.pas';
+  eInterface.Model.PessoaFisica in 'eInterface.Model.PessoaFisica.pas',
+  eInterface.Model.PessoaJuridica in 'eInterface.Model.PessoaJuridica.pas',
+  eInterface.Model.Pessoa.Factory in 'eInterface.Model.Pessoa.Factory.pas',
+  eInterface.Controller.Pessoa in 'eInterface.Controller.Pessoa.pas',
+  eInterface.Controller.Interfaces in 'eInterface.Controller.Interfaces.pas';
 
 {$R *.res}
 
